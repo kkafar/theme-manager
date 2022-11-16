5cc6676870ea7e29d9e0f520b5e3b3a9c9d439d0
Kacper Kafara <kacperkafara@gmail.com>
2022-11-16 13:59:07 +0100

    Add basic CLI

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
+
+use std::path::PathBuf;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Cli {
+	/// Path to config file - see project readme for config file description
+	#[arg(long, value_name = "FILE")]
+	config: PathBuf,
+
+	/// Run in verbose mode
+	#[arg(short, long)]
+	verbose: Option<bool>,
+
+	#[command(subcommand)]
+	command: Commands
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+	// Sets theme by name or basing on current time
+	Set {
+		/// Name of the theme to apply
+		name: Option<String>
+	}
+}
+
 fn main() {
-	println!("Hello, world!");
+	let cli = Cli::parse();
+	println!("Hello world");
 }
 
 
