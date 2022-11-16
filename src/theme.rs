4fbd4fcbe81480fb750a17217568d57fe4168614
Kacper Kafara <kacperkafara@gmail.com>
2022-11-16 23:45:47 +0100

    Parse time

--- src/theme.rs
+++ src/theme.rs
@@ -1,5 +1,6 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, error::Error, string::ParseError, num::ParseIntError};
 
+use chrono::{DateTime, Utc};
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
@@ -14,7 +15,43 @@ pub struct ThemeSpec {
 }
 
 #[derive(Deserialize, Debug)]
+pub struct TimeSpec {
+	hour: i32,
+	minutes: i32,
+}
+
+// FIXME: Don't rely on happy path
+impl From<String> for TimeSpec {
+	fn from(data: String) -> Self {
+		let mut dets = data.split(':').map(|t| t.parse::<i32>().unwrap());
+		TimeSpec { hour: dets.next().unwrap(), minutes: dets.next().unwrap() }
+	}
+}
+
+#[derive(Deserialize, Debug)]
+pub struct TimeSpan {
+	#[serde(with = "timespec")]
+	start: TimeSpec,
+	#[serde(with = "timespec")]
+	stop: TimeSpec,
+}
+
+#[derive(Deserialize, Debug)]
 pub struct Theme {
 	pub name: String,
 	pub spec: ThemeSpec,
+	pub span: Option<TimeSpan>,
+}
+
+mod timespec {
+	use serde::{Deserializer, Deserialize};
+	use super::TimeSpec;
+
+	pub fn deserialize<'de, D>(deserializer: D) -> Result<TimeSpec, D::Error>
+	where
+		D: Deserializer<'de>
+	{
+		let s = String::deserialize(deserializer)?;
+		Ok(TimeSpec::from(s))
+	}
 }
