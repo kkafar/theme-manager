9b66ff198a2a106e82640715f90d7d2945c18daf
Kacper Kafara <kacperkafara@gmail.com>
2022-11-18 23:55:45 +0100

    Add rest of error handling

--- src/gsettings.rs
+++ src/gsettings.rs
@@ -30,62 +30,152 @@ fn set_desktop(theme: &str) {
 
 fn set_mouse(theme: &str) {
 	log::info!("Setting mouse theme: {}", theme);
-	let _result = Command::new("gsettings")
+	let result = Command::new("gsettings")
 		.arg("set")
 		.arg("org.cinnamon.desktop.interface")
 		.arg("cursor-theme")
 		.arg(theme)
 		.status();
+
+	match result {
+		Ok(status) => {
+			if status.success() {
+				info!("Mouse theme set to: {}", theme);
+			} else if let Some(ret_code) = status.code() {
+				error!("Failed to set mouse theme to: {}. Process returned non-zero return code: {}", theme, ret_code);
+			} else {
+				error!("Failed to set mouse theme to: {}. Process was most likely interrupted", theme);
+			}
+		}
+		Err(err) => {
+			error!("Failed to execute the process with error: {}", err);
+		}
+	}
 }
 
 fn set_controls(theme: &str) {
 	log::info!("Setting controls theme: {}", theme);
-	let _result = Command::new("gsettings")
+	let result = Command::new("gsettings")
 		.arg("set")
 		.arg("org.cinnamon.desktop.interface")
 		.arg("gtk-theme")
 		.arg(theme)
 		.status();
+
+	match result {
+		Ok(status) => {
+			if status.success() {
+				info!("Controls theme set to: {}", theme);
+			} else if let Some(ret_code) = status.code() {
+				error!("Failed to set controls theme to: {}. Process returned non-zero return code: {}", theme, ret_code);
+			} else {
+				error!("Failed to set controls theme to: {}. Process was most likely interrupted", theme);
+			}
+		}
+		Err(err) => {
+			error!("Failed to execute the process with error: {}", err);
+		}
+	}
 }
 
 fn set_icons(theme: &str) {
 	log::info!("Setting icons theme: {}", theme);
-	let _result = Command::new("gsettings")
+	let result = Command::new("gsettings")
 		.arg("set")
 		.arg("org.cinnamon.desktop.interface")
 		.arg("icon-theme")
 		.arg(theme)
 		.status();
+
+	match result {
+		Ok(status) => {
+			if status.success() {
+				info!("Icons theme set to: {}", theme);
+			} else if let Some(ret_code) = status.code() {
+				error!("Failed to set icons theme to: {}. Process returned non-zero return code: {}", theme, ret_code);
+			} else {
+				error!("Failed to set icons theme to: {}. Process was most likely interrupted", theme);
+			}
+		}
+		Err(err) => {
+			error!("Failed to execute the process with error: {}", err);
+		}
+	}
 }
 
 fn set_borders(theme: &str) {
 	log::info!("Setting borders theme: {}", theme);
-	let _result = Command::new("gsettings")
+	let result = Command::new("gsettings")
 		.arg("set")
 		.arg("org.cinnamon.desktop.wm.preferences")
 		.arg("theme")
 		.arg(theme)
 		.status();
+
+	match result {
+		Ok(status) => {
+			if status.success() {
+				info!("Borders theme set to: {}", theme);
+			} else if let Some(ret_code) = status.code() {
+				error!("Failed to set borders theme to: {}. Process returned non-zero return code: {}", theme, ret_code);
+			} else {
+				error!("Failed to set borders theme to: {}. Process was most likely interrupted", theme);
+			}
+		}
+		Err(err) => {
+			error!("Failed to execute the process with error: {}", err);
+		}
+	}
 }
 
 fn set_wallpaper(path: &str) {
 	log::info!("Setting wallpaper to: {}", path);
-	let _result = Command::new("gsettings")
+	let result = Command::new("gsettings")
 		.arg("set")
 		.arg("org.cinnamon.desktop.background")
 		.arg("picture-uri")
 		.arg(path)
 		.status();
+
+	match result {
+		Ok(status) => {
+			if status.success() {
+				info!("Wallpaper set to: {}", path);
+			} else if let Some(ret_code) = status.code() {
+				error!("Failed to set wallpaper to: {}. Process returned non-zero return code: {}", path, ret_code);
+			} else {
+				error!("Failed to set wallpaper to: {}. Process was most likely interrupted", path);
+			}
+		}
+		Err(err) => {
+			error!("Failed to execute the process with error: {}", err);
+		}
+	}
 }
 
 fn set_kitty(theme: &str) {
 	log::info!("Setting kitty theme: {}", theme);
-	let _result = Command::new("kitty")
+	let result = Command::new("kitty")
 		.arg("+kitten")
 		.arg("themes")
 		.arg("--reload-in=all")
 		.arg(theme)
 		.status();
+
+	match result {
+		Ok(status) => {
+			if status.success() {
+				info!("Kitty theme set to: {}", theme);
+			} else if let Some(ret_code) = status.code() {
+				error!("Failed to set kitty theme to: {}. Process returned non-zero return code: {}", theme, ret_code);
+			} else {
+				error!("Failed to set kitty theme to: {}. Process was most likely interrupted", theme);
+			}
+		}
+		Err(err) => {
+			error!("Failed to execute the process with error: {}", err);
+		}
+	}
 }
 
 pub fn set_theme(theme: &Theme) {
