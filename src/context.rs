ddb25eca55e506bcefb3373d0800486cc648cccc
Kacper Kafara <kacperkafara@gmail.com>
2023-06-24 15:50:10 +0200

    Clippy

--- src/context.rs
+++ src/context.rs
@@ -11,6 +11,7 @@ impl Context {
     pub fn new(data: DataRepo) -> Self { Self { data } }
 }
 
+#[allow(clippy::derivable_impls)]
 impl Default for Context {
     fn default() -> Self {
         Self { data: DataRepo::default() }
