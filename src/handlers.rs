72da4e454fd81e62c3f551b6323352f509fc4cb9
Kacper Kafara <kacperkafara@gmail.com>
2023-06-24 13:51:07 +0200

    Improve error handling & logging

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,11 +1,24 @@
-use std::{process::Command, path::Path};
+use std::{path::Path, process::Command};
+
+use log::{info, warn};
 
 pub fn handle_edit_cmd(editor: &str, config_path: &Path) {
+    info!("Handling edit cmd with editor: {} for config: {}", editor, config_path.clone().to_str().unwrap_or("Failed to parse config path"));
     if editor.is_empty() {
+        warn!("Handling interrupted due to empty editor path");
         return;
     }
 
-    Command::new(editor)
-      .arg(config_path)
-      .status();
+    match Command::new(editor).arg(config_path).status() {
+        Ok(status) => {
+            if status.success() {
+                info!("Editor closed properly with status code 0");
+            } else {
+                warn!("Editor closed with error status code {} (-1 means unknown)", status.code().unwrap_or(-1));
+            }
+        },
+        Err(err) => {
+            warn!("Failed to open the editor, reported error: {}", err);
+        }
+    }
 }
